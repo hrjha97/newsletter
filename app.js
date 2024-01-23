6b9456e7a25976a15390eebb0315e8341c512862
hrjha97 <jha.harsh837@gmail.com>
2024-01-24 02:02:13 +0530

    updating app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const app = express();
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
@@ -29,10 +30,10 @@ app.post("/",function(request,res){
         ]
     };
     const jsonData = JSON.stringify(data);
-    const url ="https://us13.api.mailchimp.com/3.0/lists/786613c223"; 
+    const url = process.env.URL; 
     const options = {
         method : "POST",
-        auth : "Harsh:eb18494d3222df8db669aba217ac04f6-us13"
+        auth : process.env.AUTH
 
 
     }
@@ -70,8 +71,3 @@ app.listen(process.env.PORT||3000,function(){
     console.log("Server is running on port 3000");
 })
 
-// Api Key
-// eb18494d3222df8db669aba217ac04f6-us13
-
-// list id
-// 786613c223.
